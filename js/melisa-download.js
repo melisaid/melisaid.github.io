e31da0eaeab545da759894f0247cd3875e525d14
Yuda Sukmana <yudasukmana1992@gmail.com>
2017-12-08 14:34:14 +0700

    update

--- js/melisa-download.js
+++ js/melisa-download.js
@@ -1,18 +1,22 @@
 
 (function ($) {
     "use strict"; // Start of use strict
-
+    // change year
+    var d = new Date();
+    var n = d.getFullYear();
+    $('.year').text(n);
+    // github release
     $.get('https://api.github.com/repos/melisaid/melisa/releases/latest', function (data) {
 
         var asset = {}
         var html = ``
-        if (platform.os.family === 'OS X') {
+        if (platform.os.family == 'OS X') {
 
             html = `
             <i class="fa fa-apple" aria-hidden="true"></i> Download For MacOS (${data.tag_name})
             `
             asset = data.assets.find(item => item.name.includes('dmg'))
-        } else if (platform.os.family === 'Windows') {
+        } else if (platform.os.family == 'Windows' || platform.os.family == 'Windows XP') {
             html = `
             <i class="fa fa-apple" aria-hidden="true"></i> Download For Windows ${platform.os.architecture} (${data.tag_name})
             `
